9511b65b386f2ea7e263b1275df7dab1b24898b8
bwmmq <119604387+bwmmq@users.noreply.github.com>
2023-01-25 14:50:34 +0700

    Update themeConfig.js

--- themeConfig.js
+++ themeConfig.js
@@ -9,7 +9,7 @@ export const $themeBreakpoints = {}
 // APP CONFIG
 export const $themeConfig = {
   app: {
-    appName: 'sxlzptprjkt', // Will update name in navigation menu (Branding)
+    appName: 'Perfect', // Will update name in navigation menu (Branding)
     // eslint-disable-next-line global-require
     appLogoImage: '/logox.svg', // Will update logo in navigation menu (Branding)
   },
