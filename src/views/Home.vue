0fb0b842e3fef355883d4b7757121b754cfb6458
Nodine <119604387+NodineID@users.noreply.github.com>
2023-02-19 18:31:53 +0700

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,7 +9,7 @@
         <h1
           class="text-primary display-4 font-weight-bolder d-none d-md-block"
         >
-          Perfect Explorer<small class="flow-left">Beta</small>
+          Nodine Explorer<small class="flow-left">Beta</small>
         </h1>
       </div>
     </b-link>
